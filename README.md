10ba12b661f3523a7c373ec4431edcd4fd20587c
Pascal Pfiffner <phase.of.matter@gmail.com>
2015-09-09 22:54:26 +0200

    Add content submodule. Add build script.

---
+++ README.md
@@ -0,0 +1,10 @@
+C Tracker Website
+=================
+
+C Tracker website, in plain HTML + CSS.
+
+Generation
+----------
+
+Although this repository contains the final website, some pages are created from content and templates.
+The script `build.py` takes care of that; make sure you checkout the submodules before running it.
---
+++ build.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+#
+#  Uses HTML data from c-tracker-content and renders them, using Jinja2, into CTracker/HTMLContent/
+#
+#  Requirements:
+#  - jinja2
+
+import io
+from jinja2 import Environment, PackageLoader
+
+
+env = Environment(loader=PackageLoader(__name__, 'templates'))
+app_template = env.get_template('base.html')
+
+# index
+with io.open('content/index.html', 'r', encoding="utf-8") as handle:
+	content = handle.read()
+	app_template.stream(title="C Tracker", content=content) \
+		.dump('./index.html')
+
+# Privacy Policy
+with io.open('c-tracker-content/PrivacyPolicy.html', 'r', encoding="utf-8") as handle:
+	content = handle.read()
+	app_template.stream(title="Privacy Policy", content=content) \
+		.dump('./PrivacyPolicy.html')
